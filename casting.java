7bc26dede61bdaf5dad685a634a6622e23c92f90
john-shalamon <johnshalamon.cs22@stellamaryscoe.edu.in>
2025-06-17 10:30:51 +0530

    casting

---
+++ casting.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class casting{
+    public static void main (String args[])
+    {
+        int a=10;
+        double b=a;
+        System.out.println("Int"+a);
+        System.out.println("Double"+b);
+
+    }
+}
