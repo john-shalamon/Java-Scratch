d41c1aff9b8fb42867d4071b0e54b92d608130b5
john-shalamon <johnshalamon.cs22@stellamaryscoe.edu.in>
2025-06-17 12:02:49 +0530

    Add logical class with main method to determine greater value between two integers

---
+++ logical.java
@@ -0,0 +1,11 @@
+public class logical {
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+         int c=a>b ? a:b;
+        System.out.println("Greater value is: " + c);
+        
+       
+    }
+}
