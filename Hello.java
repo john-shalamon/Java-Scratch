4e984527e13177acf1a35b2ba4336aba479137e4
john-shalamon <johnshalamon.cs22@stellamaryscoe.edu.in>
2025-06-17 10:22:44 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+class Hello{
+    public static void main(String args[])
+    {
+        String name="john shalamon";
+        int age=20;
+        char gender='m';
+        float height=6.5f;
+        System.out.println ("Name:"+name);
+        System.out.println ("Age:"+age);
+        System.out.println ("Height:"+height);
+        System.out.println("Gender:"+gender);
+
+
+        
+    }
+};
+
+
