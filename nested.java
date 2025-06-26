819d7ed3c9fd0c499a820b5820f4a5147a086d39
john-shalamon <johnshalamon.cs22@stellamaryscoe.edu.in>
2025-06-26 19:58:27 +0530

    Refactor nested class to implement insurance eligibility checks based on marital status, gender, and age

--- nested.java
+++ nested.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
+
 public class nested {
     public static void main(String[] args) {
-        int age ;
-        boolean hasLicense = true;
-        boolean hasInsurance = true;
-        Scanner in=new Scanner(System.in);
-        System.out.print("Enter your age: ");
-        age = in.nextInt();
-        if (age >= 18) {
-            System.out.println("You are an adult.");
-            
-            if (hasLicense) {
-                System.out.println("You have a driver's license.");
-                
-                if (hasInsurance) {
-                    System.out.println("You can drive legally!");
-                } else {
-                    System.out.println("You need insurance to drive.");
-                }
+        int age = 0;
+        char gender = ' ';
+        char maritalStatus;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter your marital Status M/U:");
+        maritalStatus = in.next().charAt(0);
+
+        if (maritalStatus == 'u' || maritalStatus == 'U') {
+            System.out.println("Enter your gender M/F:");
+            gender = in.next().charAt(0);
+
+            System.out.println("Enter your age:");
+            age = in.nextInt();
+
+            if ((gender == 'm' || gender == 'M') && (age >= 30)) {
+                System.out.println("You are eligible for the insurance");
+            } else if ((gender == 'f' || gender == 'F') && (age >= 25)) {
+                System.out.println("You are eligible for the insurance");
             } else {
-                System.out.println("You need a driver's license to drive.");
+                System.out.println("Invalid input");
             }
         } else {
-            System.out.println("You are too young to drive.");
+            System.out.println("You are not eligible for the insurance");
         }
     }
 }
+// This code checks eligibility for insurance based on marital status, gender, and age.
